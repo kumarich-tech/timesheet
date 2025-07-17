0a462b9ef6fbbda5e7c7a9715e09f30ea6d59409
Артур <starpeich@gmail.com>
2025-07-17 15:58:58 +0300

    Fix salary export URLs

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
-from .views import timesheet_view, export_timesheet_xlsx, services_view, export_services_xlsx, export_salary_report_xlsx, report_view, export_salary_full_xlsx, export_salary_advance_xlsx
+from .views import (
+    timesheet_view,
+    export_timesheet_xlsx,
+    services_view,
+    export_services_xlsx,
+    export_salary_report_xlsx,
+    report_view,
+    export_salary_full_xlsx,
+    export_salary_advance_xlsx,
+)
 
 urlpatterns = [
     path("timesheet/", timesheet_view, name="timesheet"),
@@ -9,5 +18,5 @@ urlpatterns = [
     path("report/export/", export_salary_report_xlsx, name="export_salary_report"),
     path("report/", report_view, name="report"),
     path("export-advance/", export_salary_advance_xlsx, name="export_salary_advance"),
-    path("export-salary/", export_salary_full_xlsx, name="export_salary_report"),
+    path("export-salary/", export_salary_full_xlsx, name="export_salary_full"),
 ]
