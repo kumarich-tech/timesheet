c1e92a463bc4c3b995ae47d7e1e10f23a8ff280b
kumarich-tech <starpeich@gmail.com>
2025-07-18 13:22:12 +0000

    fix: правки перед rebase

--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Directory where uploaded media files are stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
